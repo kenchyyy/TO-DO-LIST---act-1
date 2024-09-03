9fb5735f846738cca9dc9c69a609563e0bc67217
Kenneth Joel Urbano <kenneth.urbano0127@gmail.com>
2024-09-03 13:42:59 +0800

    fixed problem where white space is submitted to the list

--- to-do-list/main.ts
+++ to-do-list/main.ts
@@ -5,33 +5,35 @@ const list = document.getElementById("list") as HTMLUListElement
 const form = document.getElementById("submit") as HTMLFormElement
 
 form.addEventListener("submit", e => {      //waits for the submit action and then adds the task, either user presses the enter button or presses the add button
-  e.preventDefault()        // prevents the page from refreshing when submit action is done
-  addTask()
+  e.preventDefault();        // prevents the page from refreshing when submit action is done
+  addTask();
 })
 
+
 function addTask(): void {
-  
-  if (input.value == '' || input.value == null) return  //to handle cases where an empty input is submitted
+  const task = input.value.trim()
+  if (task === '' || task === null) return  //to handle cases where an empty input is submitted
 
   const item = document.createElement("li");
-  const del = document.createElement("span")
+  const del = document.createElement("span");
   item.innerHTML = input.value;
-  del.innerHTML = "\u00d7"
-  list.appendChild(item)            //puts the li element into the list
-  item.appendChild(del)             //attaches the delete icon to the LI element created
-  input.value = ""
+  del.innerHTML = "\u00d7";
+  list.appendChild(item);            //puts the li element into the list
+  item.appendChild(del);             //attaches the delete icon to the LI element created
+  input.value = "";
 }
 
 //handles the completion and deletion functions
 list.addEventListener("click",(e) => {
-  const clicked = e.target
+  const clicked = e.target;
   if (clicked instanceof HTMLLIElement) {             //checks to see if the one clicked is the list element
-    clicked.classList.toggle("done")                  //toggles the class of the item to done when clicked and vice versa
+    clicked.classList.toggle("done");                  //toggles the class of the item to done when clicked and vice versa
   }
   else if(clicked instanceof HTMLSpanElement) {       //checks to see if the clicked event is a span or in this case 'x'
-    clicked.parentElement!.remove()                   //removes the LI element it was attached to
+    clicked.parentElement!.remove();                   //removes the LI element it was attached to
   }
 })
+
 
 
 
